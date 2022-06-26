6e6b0502539737f55ef5a852e988fdd6fd188e9a
Zorex <zorex@brokerengine.com.au>
2022-06-26 17:46:22 +0800

    Add slug field

--- schemas/post.js
+++ schemas/post.js
@@ -1,17 +1,28 @@
 export default {
-    name: 'post',
-    type: 'document',
-      title: 'Post',
-    fields: [
-      {
-        name: 'title',
-        type: 'string',
-        title: 'Title'
+  name: "post",
+  type: "document",
+  title: "Post",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Title",
+    },
+    {
+      name: "content",
+      type: "text",
+      title: "Content",
+    },
+    {
+      title: "Slug",
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 200, // will be ignored if slugify is set
+        slugify: (input) =>
+          input.toLowerCase().replace(/\s+/g, "-").slice(0, 200),
       },
-      {
-        name: 'content',
-        type: 'text',
-        title: 'Content'
-      },
-    ]
-  }
+    },
+  ],
+};
